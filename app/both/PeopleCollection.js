f7806f3b06a43f495be1e74ff37030f527a1b415
Philip Johnson <johnson@hawaii.edu>
2021-03-09 07:17:15 -1000

    Update for Spring 2021

--- app/both/PeopleCollection.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const people = new Mongo.Collection('People');
--- app/server/PeopleInitialization.js
+++ app/server/PeopleInitialization.js
@@ -1,9 +1,7 @@
-import { people } from '../both/PeopleCollection.js';
+import { PeopleCollection } from '../imports/api/PeopleCollection.js';
 
-if (people.find().count() === 0) {
-  people.insert({ first: 'Philip', last: 'Johnson', age: 60, city: 'Kailua' });
-  people.insert({ first: "Joanne", last: "Amberg", age: 58, city: "Kailua" });
-  people.insert({ first: "Jenna", last: "Corin", age: 31, city: "Boulder Creek" });
-  people.insert({ first: "Katie", last: "Kai", age: 25, city: "Palo Alto" });
-  people.insert({ first: "Doris", last: "Johnson", age: 93, city: "Naples" });
-}
+PeopleCollection.insert({ first: 'Philip', last: 'Johnson', age: 60, city: 'Kailua'});
+PeopleCollection.insert({ first: 'Joanne', last: 'Amberg', age: 58, city: 'Kailua' });
+PeopleCollection.insert({ first: 'Jenna', last: 'Corin', age: 31, city: 'Boulder Creek' });
+PeopleCollection.insert({ first: 'Katie', last: 'Kai', age: 25, city: 'Palo Alto' });
+PeopleCollection.insert({ first: 'Doris', last: 'Johnson', age: 93, city: 'Naples' });
